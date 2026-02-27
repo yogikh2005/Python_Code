8ed576516d7db2ace757a63f32d98b9568446ead
yogikh2005 <yogikh2018@gmail.com>
2026-02-27 12:00:00 +0530

    27/2/26

---
+++ Assignment_40/A40_5.py
@@ -0,0 +1,221 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (accuracy_score,confusion_matrix,ConfusionMatrixDisplay,classification_report)
+
+########################################################## 
+
+# Step 1 : Load Dataset
+
+##########################################################
+Border="-"*40
+
+print(Border)
+print("Step 1 : Load Dataset")
+print(Border)
+
+Datasetpath="student_performance_ml.csv"
+
+df=pd.read_csv(Datasetpath) #  we load the datset from student_performance_ml.csv
+
+print("Dataset loaded successfully")
+
+
+############################################################    
+
+# Step 2: Data Analysis(EDA)
+
+############################################################ 
+
+print(Border)
+print("Step 2: Data Analysis(EDA)")
+print(Border)
+
+print("Shape of dataset : ",df.shape)          # we check shape of datset
+print("Columns of dataset : ",df.columns)      # check columns info
+
+print("Missing values : ", )
+print(df.isnull().sum())  # check the null value present in dataset or not 
+
+print("class ditribution : ")
+print(df["FinalResult"].value_counts()) # depentdend varivale class value count
+
+print("Statical report of dataset :")
+print(df.describe())
+
+#############################################################
+
+# Step 3 : Decide Independent & Dependet Varible
+
+#############################################################
+
+print(Border)
+print("Step 3 : Decide Independent & Dependet Varible")
+print(Border)
+
+feacture_cols=[
+    "StudyHours",
+    "Attendance",
+    "PreviousScore",
+    "AssignmentsCompleted",
+    "SleepHours" 
+]                                         # decide the Independent varible
+
+X=df[feacture_cols] 
+Y=df["FinalResult"]   # decide the Dependet varible
+
+print("X shape : ",X.shape)         # check shape of Independent varible
+print("Y shape : ",Y.shape)        # check shape of Dependet varible
+
+##############################################################
+
+# Step 4 : Visualisation of dataset
+
+print(Border)
+print("Step 4 : Visualisation of dataset")
+print(Border)
+
+plt.figure(figsize=(7,5))
+
+sns.scatterplot(
+    x="StudyHours",
+    y="Attendance",
+    hue="FinalResult",
+    data=df
+)                                               # plot the scatterplot for studyhours vs Attendance with Finalresult
+
+plt.title("StudyHours vs Attendance")
+#plt.show()
+
+#########################################################
+
+# Step 5 : Split the dataset for training and testing
+
+print(Border)
+print("Step 5 : Split the dataset for training and testing")
+print(Border)
+
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=42,test_size=0.2)  # split the datset in  X_train,X_test,Y_train,Y_test , splait data with 80:20
+                                                                                    # random_state for the shuffel dataset
+print("X_train : ",X_train.shape)
+print("X_test : ",X_test.shape)
+print("Y_train : ",Y_train.shape)
+print("Y_test : ",Y_test.shape)
+
+######################################################
+
+# Step 6 : Build the model
+
+print(Border)
+print("Step 6 : Build the model")
+print(Border)
+
+model=DecisionTreeClassifier(               # create the model obj with max_depth=2,we use DecisionTreeClassifier
+                                            # which supriviwed with classfication algo
+    criterion="gini",
+    max_depth=2,
+    random_state=42
+)
+                                             
+print("Model successfully created : ",model)
+
+####################################################
+    
+# Step 7 : Train the model
+
+print(Border)
+print("Step 7 : Train the model")
+print(Border)
+
+model.fit(X_train,Y_train)                         # train the model
+
+print("model training completed")
+
+####################################################
+
+# Step 8 : Evaluate the model
+
+print(Border)
+print("Step 8 : Evaluate the model")
+print(Border)
+
+Y_pred=model.predict(X_test)                                # test the model
+
+print("Model evulation(Testing) completed")
+print("Y_pred shape :" , Y_pred.shape)                      # displat shape of Y_pred
+
+print("Predicated ans :")
+print(Y_pred)
+
+print("Actual ans :")
+print(Y_test)
+
+
+
+
+###################################################
+
+# User defined accuracy_score(Y_pred,Y_test)
+# Accuracy=(TP+TN)/(FP+FN+TP+TN​)
+
+def accuracy_scoreX(Y_pred, Y_test):
+    TP = 0
+    TN = 0
+    FP = 0
+    FN = 0
+
+    Y_test = Y_test.values  
+
+    for i in range(len(Y_test)):
+
+        if Y_test[i] == 1 and Y_pred[i] == 1:
+            TP += 1
+
+        elif Y_test[i] == 0 and Y_pred[i] == 0:
+            TN += 1
+
+        elif Y_test[i] == 0 and Y_pred[i] == 1:
+            FP += 1
+
+        elif Y_test[i] == 1 and Y_pred[i] == 0:
+            FN += 1
+
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+
+    print("Accuracy:", accuracy)
+
+    return accuracy
+
+####################################################
+
+# Step 9 :  Evaluate the model performance
+
+print(Border)
+print("Step 9 : Evaluate the model performance : ")
+print(Border)
+
+accuracy=accuracy_scoreX(Y_pred,Y_test)                          # calculate testing accuracy of the train model
+print("Testing Accuracy(User Define) of the model is : ",accuracy*100) 
+      
+
+accuracy=accuracy_score(Y_pred,Y_test)                          # calculate testing accuracy of the train model
+print("Testing Accuracy of the model is : ",accuracy*100) 
+
+cm=confusion_matrix(Y_pred,Y_test)                             # display the confusion matrix of Y_pred & Y_test 
+print("Confusion matrix : \n",cm)
+
+print("Classification report : ")
+print(classification_report(Y_test,Y_pred))      # display the testing report
+
+
+##################################################################################
+
+'''
+Explanation : 
+
+            User Defined accuracy_scoreX is same as built-in accuracy_score
+
+'''
